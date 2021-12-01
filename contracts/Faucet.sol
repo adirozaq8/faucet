287d6a33db3180adc9b1903d2c5d85f4ef43b682
adirozaq8 <reza.gerrard@gmail.com>
2021-12-01 18:14:00 +0700

    create new smart contract

---
+++ contracts/Faucet.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+    // storage variables
+    uint public funds = 1000;
+    int public counter = -525;
+    uint32 public _uint32 = 1923695649;
+}
