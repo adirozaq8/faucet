c897c49f9ee9fc2f71cc73659055d650d5a69cb9
adirozaq8 <reza.gerrard@gmail.com>
2021-12-02 17:06:10 +0700

    create regular function

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,6 +2,12 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    
+
     receive() external payable {}
+    function addFunds() external payable {}
 }
+
+// const instance = await Faucet.deployed()
+// instance.addFunds({value: "2000000000000000000"})
+
+// web3.eth.sendTransaction({from: address, to: address/smart_contract, data: hashed_keccak256_function (4 bytes), value: value_in_wei})
