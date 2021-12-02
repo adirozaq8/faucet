4f68a2162aa61b3cbfc7961b827d785633fb8962
adirozaq8 <reza.gerrard@gmail.com>
2021-12-02 15:52:40 +0700

    test send transaction to smart contract

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,8 +2,6 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables
-    uint public funds = 1000;
-    int public counter = -525;
-    uint32 public _uint32 = 1923695649;
+    
+    receive() external payable {}
 }
