43618a26a37d33019e5366bc1d60ce9c0ae3ae7e
Rozaq <rozaqadi@baezeni.com>
2021-12-04 20:05:14 +0700

    change to mapping

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -3,20 +3,27 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
 
-    address[] public funders;
+    uint public numOfFunders;
+    mapping(uint => address) private funders;
 
     receive() external payable {}
     function addFunds() external payable {
-        funders.push(msg.sender);
+        uint index = numOfFunders++;
+        funders[index] = msg.sender;
     }
 
-    function getAllFunders() public view returns(address[] memory) {
-        return funders;
+    function getAllFunders() external view returns (address[] memory) {
+        address[] memory _funders = new address[](numOfFunders);
+
+        for(uint i = 0; i < numOfFunders; i++) {
+            _funders[i] = funders[i];
+        }
+
+        return _funders;
     }
 
     function getFunderAtIndex(uint index) external view returns(address) {
-        address[] memory _funders = getAllFunders();
-        return _funders[index];
+        return funders[index];
     }
 
     /*
